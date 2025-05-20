0e3909259610e5d3dbb8ce437151c178dfa67d25
samrat <samrat.subratadas@gmail.com>
2025-05-20 23:39:32 +0530

    eoor done

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -1,10 +1,9 @@
-import Sidebar from "../components/Sidebar";
+
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="full-body-parts">
-      <Sidebar/>
       <main className="d-block ms-lg-auto content-parts d-block">
             <Navbar/>
 
