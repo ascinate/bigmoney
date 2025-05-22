bd70dad79848d76325ddd5a391e369a1f01b8041
subrata <subrata@ascinatetech.com>
2025-05-22 16:31:37 +0530

    new edits

--- src/app/components/Schedule.js
+++ src/app/components/Schedule.js
@@ -7,22 +7,15 @@ import { IoMdClose } from "react-icons/io";
 import CalendarWithDropdown from './CalendarWithDropdown';
 
 function Schedule() {
-    const [viewModal, setViewModal] = useState(false)
 
-    const handleAddDay = () => {
-        console.log("hello");
-        setViewModal(true)
-    }
 
     return(
         <>
-                     {/* week section start */}
             <section className='general-week-section'>
                 <div className='wrapper'>
                     <div className='general-week-container'>
                         <div className='' id='general-weekly-schedule'>General Weekly Schedule</div>
                         <div className='general-weekly-schedule-bottom'>
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -36,7 +29,6 @@ function Schedule() {
                                     <TbPencilMinus className='day-name-time-icon' />
                                 </div>
                             </div>
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -51,7 +43,6 @@ function Schedule() {
                                 </div>
                             </div>
 
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -65,7 +56,6 @@ function Schedule() {
                                     <TbPencilMinus className='day-name-time-icon' />
                                 </div>
                             </div>
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -79,8 +69,6 @@ function Schedule() {
                                     <TbPencilMinus className='day-name-time-icon' />
                                 </div>
                             </div>
-
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -94,8 +82,6 @@ function Schedule() {
                                     <TbPencilMinus className='day-name-time-icon' />
                                 </div>
                             </div>
-
-                            {/* day */}
                             <div id='week-days'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -109,7 +95,6 @@ function Schedule() {
                                     <TbPencilMinus className='day-name-time-icon' />
                                 </div>
                             </div>
-                            {/* day */}
                             <div id='week-days-sunday'>
                                 <div className='day-name-time'>
                                     <label className="switch">
@@ -132,11 +117,6 @@ function Schedule() {
                     </div>
                 </div>
             </section>
-
-            {/* week section end */}
-
-            {/* day section start */}
-
             <section className='general-week-section'>
                 <div className='wrapper'>
                     <div className='general-week-container'>
@@ -162,7 +142,6 @@ function Schedule() {
 
 
                             <div className={` ${viewModal ? 'show' : 'modal'}`}>
-                                {/* <p>i am modal</p> */}
 
 
                                 <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -172,7 +151,6 @@ function Schedule() {
                                     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                             <section className='calender'>
-                                                {/* row-1 */}
                                                 <article className='Custom-schedule-row-one art'>
                                                     <div className=''>
                                                         <h2 className='calender-heading'>Custom schedule</h2>
@@ -182,7 +160,6 @@ function Schedule() {
                                                     </div>
                                                 </article>
 
-                                                {/* row-2 */}
                                                 <article className='Custom-schedule-row-two art'>
                                                     <div className='Custom-schedule-row-two-select'>
                                                         <select name="" className="Custom-schedule-select">
@@ -193,7 +170,6 @@ function Schedule() {
                                                     </div>
                                                 </article>
 
-                                                {/* row-3 */}
                                                 <article className='Custom-schedule-row-three'>
                                                     <div className='pick-date'>
                                                         <p className='pick-text'>Pick a Date</p>
@@ -223,7 +199,6 @@ function Schedule() {
                                                         </div>
                                                     </div>
                                                 </article>
-                                                {/* row-4 */}
                                                 <CalendarWithDropdown/>
                                             </section>
                                         </div>
@@ -236,10 +211,6 @@ function Schedule() {
                     </div>
                 </div>
             </section>
-            {/* day section end */}
-
-
-            {/* Holidays & Days Off start*/}
             <section className='general-week-section'>
                 <div className='wrapper'>
                     <div className='general-week-container'>
@@ -272,7 +243,6 @@ function Schedule() {
 
 
                             <div className={` ${viewModal ? 'show' : 'modal'}`}>
-                                {/* <p>i am modal</p> */}
 
 
                                 <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -282,7 +252,6 @@ function Schedule() {
                                     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                             <section className='calender'>
-                                                {/* row-1 */}
                                                 <article className='Custom-schedule-row-one art'>
                                                     <div className=''>
                                                         <h2 className='calender-heading'>Custom schedule</h2>
@@ -292,7 +261,6 @@ function Schedule() {
                                                     </div>
                                                 </article>
 
-                                                {/* row-2 */}
                                                 <article className='Custom-schedule-row-two art'>
                                                     <div className='Custom-schedule-row-two-select'>
                                                         <select name="" className="Custom-schedule-select">
@@ -303,7 +271,6 @@ function Schedule() {
                                                     </div>
                                                 </article>
 
-                                                {/* row-3 */}
                                                 <article className='Custom-schedule-row-three'>
                                                     <div className='pick-date'>
                                                         <p className='pick-text'>Pick a Date</p>
@@ -333,9 +300,8 @@ function Schedule() {
                                                         </div>
                                                     </div>
                                                 </article>
-                                                {/* row-4 */}
                                                 
-                                                        <CalendarWithDropdown/>
+                                                <CalendarWithDropdown/>
                                                    
                                             </section>
                                         </div>
@@ -348,7 +314,6 @@ function Schedule() {
                     </div>
                 </div>
             </section>
-            {/* Holidays & Days end */}
         </>
     );
 }
